580c790c35582d8d1e7b46bf116214e8484a1bc0
Benas <benas.rag@gmail.com>
2022-02-11 12:21:24 +0200

    upd

--- remote.ino
+++ remote.ino
@@ -14,145 +14,116 @@ volatile unsigned long prevTime = 0;
 volatile unsigned long prevDiff = 0;
 
 void setup() {
-  Serial.begin(9600);
-
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN, LOW);
-
-  pinMode(RX_PIN, INPUT);
-
-  if (1 == MODE_READ) {
-    attachInterrupt(digitalPinToInterrupt(RX_PIN), onChange, CHANGE);
-    interrupts();
-  }
+    Serial.begin(9600);
+    
+    pinMode(PIN, OUTPUT);
+    digitalWrite(PIN, LOW);
+    
+    pinMode(RX_PIN, INPUT);
+    
+    if (1 == MODE_READ) {
+        attachInterrupt(digitalPinToInterrupt(RX_PIN), onChange, CHANGE);
+        interrupts();
+    }
 }
 
 void loop() { 
     if (0 == MODE_READ) {
-      for (int i = 0; i < 3; i++) {
-        sendCode();
-      }
+        for (int i = 0; i < 3; i++) {
+            sendCode("1011101101011011010100011");
+        }
+        
+        Serial.println("Sent");
+        delay(10000);
+    }
+}
 
-      Serial.println("Sent");
-      delay(10000);  
+void sendCode(char code[]) {
+    digitalWrite(PIN, LOW); 
+    delayMicroseconds(24000);
+    
+    int i = 0;
+    
+    while (code[i] != '\0') {
+        if ('0' == code[i]) {
+            digitalWrite(PIN, LOW); 
+            delayMicroseconds(PULSE_SHORT);
+            
+            digitalWrite(PIN, HIGH); 
+            delayMicroseconds(PULSE_LONG);
+        } else {
+            digitalWrite(PIN, LOW); 
+            delayMicroseconds(PULSE_LONG);
+            
+            digitalWrite(PIN, HIGH); 
+            delayMicroseconds(PULSE_SHORT);
+        }
+
+        i++;
     }
 }
 
 void onChange() {
-  int state = digitalRead(RX_PIN);
-
-  unsigned long currentTime = micros();
-  unsigned long diff = currentTime - prevTime;
-  prevTime = currentTime;
-  
-  if (!started && HIGH == state && diff >= SYNC_INTERVAL) {
-    started = true;
-    prevDiff = diff;
-
-    return;
-  }
-
-  if (started && LOW == state) {
-    if (currentIndex > 1 &&
-      ((diff + prevDiff) < (PULSE_SHORT + PULSE_LONG) - STRIP || (diff + prevDiff) > (PULSE_SHORT + PULSE_LONG) + STRIP)
-    ) {
-      currentIndex = 0;
-      prevDiff = diff;
-      started = false;
-      
-      return;
+    int state = digitalRead(RX_PIN);
+    
+    unsigned long currentTime = micros();
+    unsigned long diff = currentTime - prevTime;
+    prevTime = currentTime;
+    
+    if (!started && HIGH == state && diff >= SYNC_INTERVAL) {
+        started = true;
+        prevDiff = diff;
+        
+        return;
     }
     
-    if (diff <= prevDiff) {
-      code[currentIndex++] = 1;
+    if (started && LOW == state) {
+        if (
+            currentIndex > 1 &&
+            ((diff + prevDiff) < (PULSE_SHORT + PULSE_LONG) - STRIP || (diff + prevDiff) > (PULSE_SHORT + PULSE_LONG) + STRIP)
+        ) {
+            currentIndex = 0;
+            prevDiff = diff;
+            started = false;
+            
+            return;
+        }
+        
+        if (diff <= prevDiff) {
+            code[currentIndex++] = 1;
+        } else {
+            code[currentIndex++] = 0;
+        }
+        
+        prevDiff = diff;
+        
+        if (currentIndex == 25) {
+            for (int i = 0; i < 25; i++) {
+                if (i == 0) {
+                    Serial.print(1);
+                } else {
+                    Serial.print(code[i]);
+                }
+            }
+        
+            Serial.print(" || ");
+            
+            for (int i = 0; i < 25; i++) {
+                if (i == 0) {
+                    Serial.print(0);
+                } else {
+                    Serial.print(code[i]);
+                }
+            }
+            
+            Serial.println("");
+        
+            started = false;
+            prevDiff = diff;
+            diff = 0;
+            currentIndex = 0;
+        }
     } else {
-      code[currentIndex++] = 0;
+        prevDiff = diff;
     }
-
-    prevDiff = diff;
-
-    if (currentIndex == 25) {
-      for (int i = 0; i < 25; i++) {
-        if (i == 0) {
-          Serial.print(1);
-        } else {
-          Serial.print(code[i]);
-        }
-      }
-
-      Serial.print(" || ");
-
-      for (int i = 0; i < 25; i++) {
-        if (i == 0) {
-          Serial.print(0);
-        } else {
-          Serial.print(code[i]);
-        }
-      }
-
-      Serial.println("");
-      
-      started = false;
-      prevDiff = diff;
-      diff = 0;
-      currentIndex = 0;
-    }
-  } else {
-    prevDiff = diff;
-  }
-}
-
-void sendCode() {
-    //sync
-    digitalWrite(PIN, LOW); 
-    delayMicroseconds(24000);
-
-    //1011101101011011010100011
-    
-    sendOne();
-    sendZero();
-    sendOne();
-    sendOne();
-
-    sendOne();
-    sendZero();
-    sendOne();
-    sendOne();
-
-    sendZero();
-    sendOne();
-    sendZero();
-    sendOne();
-
-    sendOne();
-    sendZero();
-    sendOne();
-    sendOne();
-
-    sendZero();
-    sendOne();
-    sendZero();
-    sendOne();
-
-    sendZero();
-    sendZero();
-    sendZero();
-    sendOne();
-    sendOne();
-}
-
-void sendOne() {
-  digitalWrite(PIN, LOW); 
-  delayMicroseconds(PULSE_LONG);
-
-  digitalWrite(PIN, HIGH); 
-  delayMicroseconds(PULSE_SHORT);
-}
-
-void sendZero() {
-  digitalWrite(PIN, LOW); 
-  delayMicroseconds(PULSE_SHORT);
-
-  digitalWrite(PIN, HIGH); 
-  delayMicroseconds(PULSE_LONG);
 }
